1e53c38dabe941c48f9eb33ab256e2999d1500ae
Anders Hong <andershong@gmail.com>
2011-11-07 15:22:52 +0800

    init

---
+++ ios/taxi/taxi/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  taxi
+//
+//  Created by Anders Hong on 11-11-7.
+//  Copyright (c) 2011年 JingHong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios/taxi/taxi/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  taxi
+//
+//  Created by Anders Hong on 11-11-7.
+//  Copyright (c) 2011年 JingHong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
